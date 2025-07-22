2ad151263208136398ca7eb937ce4723bcabe64a
Matija <matijazib@gmail.com>
2025-07-22 17:28:06 +0200

    fixed navbar

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,5 @@
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
-import { Navigation } from '@/components/layout/navigation'
 
 export default async function ProtectedLayout({
   children,
@@ -15,7 +14,6 @@ export default async function ProtectedLayout({
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navigation />
       <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         {children}
       </main>

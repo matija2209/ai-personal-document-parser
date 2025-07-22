d2c97f9bb09fba8ffdbfd3eaf0df5a1422e391ba
Matija <matijazib@gmail.com>
2025-07-22 16:13:38 +0200

    removed skeleton

--- src/app/(protected)/dashboard/document/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from '@/components/ui/skeleton';
-
-export default function DocumentLoading() {
-  return (
-    <div className="container mx-auto px-4 py-8 max-w-4xl">
-      <Skeleton />
-    </div>
-  );
-}
